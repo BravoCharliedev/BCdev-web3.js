b46403a322256650d0dd9f826b4a71f4b7c3444e
bravocharliedev <anan@avesta.io>
2018-10-09 05:32:02 +0000

    Anan Update 001

--- example/node-app.js
+++ example/node-app.js
@@ -3,10 +3,10 @@
 var Web3 = require('../index.js');
 var web3 = new Web3();
 
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:11998'));
 
 var coinbase = web3.eth.coinbase;
-console.log(coinbase);
+console.log("Conin Base : " , coinbase);
 
 var balance = web3.eth.getBalance(coinbase);
-console.log(balance.toString(10));
+console.log("Balance    : " , balance.toString(10));
